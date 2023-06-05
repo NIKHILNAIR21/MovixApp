41f6f75342342ddf51c64cadd6f2e2c026041a44
NiKHIL NAIR <nikhilnair340@gmail.com>
2023-06-06 01:20:06 +0530

    PageNotFound component update

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
+import "./pageNotFound.scss";
+
+import ContentWrapper from "../../component/ContentWrapper/ContentWrapper";
+
 const PageNotFound = () => {
-  return <div>PageNotFound</div>;
+  return (
+    <div className="pageNotFound">
+      <ContentWrapper>
+        <span className="bigText">404</span>
+        <span className="smallText">Page not found!</span>
+      </ContentWrapper>
+    </div>
+  );
 };
 
 export default PageNotFound;
